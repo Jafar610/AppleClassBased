3b727c66c9b752608aaa4fbb3717d6ebb49c801c
Jafar610 <jafarmohamed955@gmail.com>
2026-03-09 22:21:43 +0300

    file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 export default class App extends Component {
   render() {
     return (
-      <>
+      <div className='outer-wrapper'>
        <Header />
-      </>
+       <Footer />
+      </div>
     )
   }
 }
