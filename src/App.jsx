a75874f64be3cd3de86e14cb7daa18c9aeefcd98
Jafar610 <jafarmohamed955@gmail.com>
2026-03-10 21:58:12 +0300

    file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import MacBook from './Components/MacBook'
 
 export default class App extends Component {
   render() {
     return (
       <div className='outer-wrapper'>
        <Header />
+       <MacBook />
        <Footer />
       </div>
     )
---
+++ src/Components/MacBook.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import macBook from '../assets/images/home/mac-laptop.jpg'
+export default class MacBook extends Component {
+  render() {
+    return (
+      <section class="main-highlight-wrapper">
+        <div class="internal-wrapper">
+          <div class="model">16-inch model</div>
+
+          <div class="product-title">MacBook Pro</div>
+
+          <div class="brief-description">The best for the brightest.</div>
+
+          <div class="links-wrapper">
+            <ul>
+              <li><a href="">Learn more</a></li>
+              <li><a href="">Buy</a></li>
+            </ul>
+          </div>
+
+          <div class="main-image-wrapper">
+            <img src={macBook} />
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
