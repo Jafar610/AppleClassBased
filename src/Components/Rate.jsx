f14dffec9b26a0dc79646e340b7c112bca11a5f5
Jafar610 <jafarmohamed955@gmail.com>
2026-03-18 22:42:08 +0300

    file updated

--- src/Components/Rate.jsx
+++ src/Components/Rate.jsx
@@ -4,7 +4,16 @@ export default class Rate extends Component {
   render() {
     return (
       <>
-      
+      <div className='container'>
+        <h1>Rate this mac out of 10</h1>
+        <div className='btn'>
+            <button>Rate Up</button>
+            <button>Rate Down</button>
+        </div>
+        <div id='textdisplay'>
+            <h2>Average Rating: 5.0</h2>
+        </div>
+      </div>
       </>
     )
   }
