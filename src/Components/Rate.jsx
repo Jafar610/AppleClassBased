8e8505a4722922f67110acf26cc432b78c93bc9f
Jafar610 <jafarmohamed955@gmail.com>
2026-03-19 21:45:33 +0300

    file updated

--- src/Components/Rate.jsx
+++ src/Components/Rate.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
 import './Rate.css'
 export default class Rate extends Component {
+  constructor(){
+    super()
+    this.state = {
+      rating: 5.0,
+    }
+  }
+
+  increment = ()=>{
+    this.setState((state)=>{
+      return{
+        rating: state.rating + 1
+      }
+    })
+  }
+
   render() {
     return (
       <>
       <div className='container'>
         <h1>Please rate this mac out of 10</h1>
         <div className='btn'>
-            <button className='btn1'>Rate Up</button>
-            <button className='btn2'>Rate Down</button>
+            <button className='btn1' onClick={this.increment}>Rate Up</button>
+            <button className='btn2' onClick={this.decrement}>Rate Down</button>
         </div>
         <div id='textdisplay'>
-            <h2>Average Rating: 5.0</h2>
+            <h2>Average Rating: {this.state.rating}</h2>
         </div>
       </div>
       </>
